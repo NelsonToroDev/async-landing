310a12a8467d250f445d058fc5994b64869ddc05
Nekero8 <nekero8@gmail.com>
2022-12-13 14:07:38 -0400

    created landing page and referencing top 4 youtube videos from a channel using RapidAPI

---
+++ src/assets/main.js
@@ -0,0 +1,41 @@
+import config from "../../config";
+const API = config.app.API;
+const content = null ||  document.getElementById('content');
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': config.app.XRapidAPIKey,//'706e763c7dmsh8adff5a7ea6da2dp1d6ac9jsn431a941ef7cf',
+		'X-RapidAPI-Host': config.app.XRapidAPIKey//'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlAPI) {
+    const response = await fetch(urlAPI, options);
+    const data = await response.json();
+    return data;
+}
+
+(async ()=>{
+    try {
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+        <div class="group relative">
+          <div
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          </div>
+          <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+              <span aria-hidden="true" class="absolute inset-0"></span>
+              ${video.snippet.title}
+            </h3>
+          </div>
+        </div>`).slice(0, 4).join('')}
+        `;
+        content.innerHTML = view;
+    } catch (error) {
+        console.log(error);
+        //content.innerHTML = '<span class="mt-4 flex justify-between">Error: the application could communicate with Youtube</span>';
+    }
+})();
